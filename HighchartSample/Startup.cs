8c0bdfd07607ea148a6aa2a3fc3cd6664bb98312
ashida <ashida@v-force.co.jp>
2016-08-02 11:24:59 +0900

    サンプルのコミット

---
+++ HighchartSample/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HighchartSample.Startup))]
+namespace HighchartSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
